1019818fb3f00fb9178c52d393ef050c9525a93d
KingOfExcuses <kingofexcusestv@gmail.com>
2023-08-27 15:23:38 -0500

    add selectAnswer and move to next question

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const questionCon = document.getElementById('question-container')
 const questionEl = document.getElementById('question')
 const choiceButton = document.querySelectorAll('.choice')
 const timer = document.getElementById('timer')
+const answer = document.getElementById('result')
 
 // Init Variables
 var currentQuestionIndex = 0
@@ -16,33 +17,34 @@ const questions = [
 	{
 		question: 'Commonly used data types do NOT include:',
 		choices: ['alerts', 'booleans', 'strings', 'numbers'],
-		correctAnswer: 0,
+		correctAnswer: 'alerts',
 	},
 	{
 		question: 'The condition in an if/else statement is enclosed with _____.',
 		choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-		correctAnswer: 2,
+		correctAnswer: 'parenthesis',
 	},
 	{
 		question: 'Arrays in JS can be used to store _____.',
 		choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-		correctAnswer: 3,
+		correctAnswer: 'all of the above',
 	},
 	{
 		question: 'String values must be enclosed within _____ when being assigned to variables.',
 		choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-		correctAnswer: 2,
+		correctAnswer: 'quotes',
 	},
 	{
 		question:
 			'A very useful tool used during developement and debugging for printing content to the debugger is:',
 		choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
-		correctAnswer: 3,
+		correctAnswer: 'console.log',
 	},
 ]
 
 // Event Listeners
 startButton.addEventListener('click', startQuiz)
+choiceButton.forEach((button) => button.addEventListener('click', selectAnswer))
 
 // Start Quiz
 function startQuiz() {
@@ -55,10 +57,33 @@ function startQuiz() {
 // Display Questions and Choices
 function displayQuestion() {
 	const question = questions[currentQuestionIndex]
+
 	questionEl.textContent = question.question
 	choiceButton.forEach((button, i) => {
 		button.textContent = question.choices[i]
 	})
+}
+
+function selectAnswer(e) {
+	const question = questions[currentQuestionIndex]
+
+	const selectedChoice = e.target
+	if (selectedChoice.textContent === question.correctAnswer) {
+		answer.textContent = 'Correct!'
+		answer.style.color = 'green'
+		score += 5
+	} else {
+		answer.textContent = 'Wrong!'
+		answer.style.color = 'red'
+		timeRemaining -= 10
+	}
+	currentQuestionIndex++
+	if (currentQuestionIndex < questions.length) {
+		displayQuestion()
+	} else {
+		// stopQuiz()
+		console.log('quiz over')
+	}
 }
 
 // Timer
@@ -68,11 +93,4 @@ function updateTimer() {
 	if (timeRemaining <= 0) {
 		clearInterval(countDown)
 	}
-	console.log(timeRemaining)
 }
-//
-// Call functions for testing
-//
-// displayQuestion()
-// startQuiz()
-// updateTimer()
