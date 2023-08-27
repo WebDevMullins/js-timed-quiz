a64c935513faeaec1733f3b23ad36efdff447e76
KingOfExcuses <kingofexcusestv@gmail.com>
2023-08-27 12:19:26 -0500

    add questions and answers to js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Questions and Answers
+const questions = [
+	{
+		question: 'Commonly used data types do NOT include:',
+		choices: ['alerts', 'booleans', 'strings', 'numbers'],
+		correctAnswer: 0,
+	},
+	{
+		question: 'The condition in an if/else statement is enclosed with _____.',
+		choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+		correctAnswer: 2,
+	},
+	{
+		question: 'Arrays in JS can be used to store _____.',
+		choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+		correctAnswer: 3,
+	},
+	{
+		question: 'String values must be enclosed within _____ when being assigned to variables.',
+		choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			'A very useful tool used during developement and debugging for printing content to the debugger is:',
+		choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+		correctAnswer: 3,
+	},
+]
